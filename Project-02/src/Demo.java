653063fc3cb898af2fafa8d718759ab1a5371a25
SupunWellappili <supunchandana1814@gmail.com>
2024-05-18 23:03:59 +0530

    Use Generics

--- Project-02/src/Demo.java
+++ Project-02/src/Demo.java
@@ -1,39 +1,36 @@
 public class Demo {
     public static void main(String[] args) {
-        Database d1 = new Database();
-        d1.put(new Dog("Tom"));
-        d1.put(new Dog("Sheba"));
-        d1.put(new Dog("Zini"));
+        Database<Dog> db1 = new Database<>(new Dog("Tom"));
 
-        Database d2 = new Database();
-        d2.put(new Cat("Tim"));
-        d2.put(new Cat("Kitty"));
-        d2.put(new Cat("Puss"));
-
+        Database<Cat> db2 = new Database<>(new Cat("Tim"));
     }
 }
 
 
-class Database {
-     void put(Dog d) {
-         System.out.println(d.name);
+class Database<T> {
+    T o1;
+
+    Database(T t) {
+        this.o1 = t;
     }
 
-    void put(Cat c){
-        System.out.println(c.name);
+    <T> T put(T d) {
+        return d;
     }
 }
 
-class Dog{
+class Dog {
     String name;
-    Dog(String name){
+
+    Dog(String name) {
         this.name = name;
     }
 }
 
-class Cat{
+class Cat {
     String name;
-    Cat(String name){
-        this.name =name;
+
+    Cat(String name) {
+        this.name = name;
     }
 }
